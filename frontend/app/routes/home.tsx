a43f7aa25a4d4795c4bbc554b13b3175a007baa4
------- <tamarjargal25@my.whitworth.edu>
2025-08-15 19:09:37 -0700

    Init front-end

---
+++ frontend/app/routes/home.tsx
@@ -0,0 +1,16 @@
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <div>
+    <text>
+      Welcome to job coach application
+    </text>
+  </div>;
+}
